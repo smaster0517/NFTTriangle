6a96f4944e489a1d1fc90363b91cfc0278721924
smaster0517 <smaster0517@gmail.com>
2021-09-15 12:14:25 -0500

    Creator dashboard page and fix my assets

--- pages/creator-dashboard.js
+++ pages/creator-dashboard.js
@@ -10,10 +10,11 @@ import {
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
 import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json';
 
-export default function MyAssets() {
+export default function CreatorDashboard() {
   const [nfts, setNfts] = useState([]);
+  const [sold, setSold] = useState([]);
+
   const [loadingState, setLoadingState] = useState('not-loaded');
-  
   useEffect(() => {
     loadNFTs();
   }, []);
@@ -26,7 +27,7 @@ export default function MyAssets() {
 
     const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, signer);
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
-    const data = await marketContract.fetchMyNFTs();
+    const data = await marketContract.fetchItemsCreated();
 
     const items = await Promise.all(data.map(async i => {
       const tokenUri = await tokenContract.tokenURI(i.tokenId);
@@ -41,17 +42,16 @@ export default function MyAssets() {
       }
       return item;
     }));
+    const soldItems = items.filter(i => i.sold);
+    setSold(soldItems);
     setNfts(items);
     setLoadingState('loaded');
   }
 
-  if (loadingState === 'loaded' && !nfts.length) return (
-    <h1 className="py-10 px-20 text-3xl">No assets owned</h1>
-  );
-
   return (
-    <div className="flex justify-center">
+    <div>
       <div className="p-4">
+        <h2 className="text-2xl py-2">Items Created</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
         {
           nfts.map((nft, i) => (
@@ -64,6 +64,27 @@ export default function MyAssets() {
           ))
         }
         </div>
+      </div>
+      <div className="px-4">
+        {
+          Boolean(sold.length) && (
+            <div>
+              <h2 className="text-2xl py-2">Items sold</h2>
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+              {
+                sold.map((nft, i) => (
+                  <div key={i} className="border shadow rounded-xl overflow-hidden">
+                    <img src={nft.image} className="rounded" />
+                    <div className="p-4 bg-black">
+                      <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
+                    </div>
+                  </div>
+                ))
+              }
+              </div>
+            </div>
+          )
+        }
       </div>
     </div>
   );
